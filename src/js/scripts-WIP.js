ac1345ceb26d12b89557bf6ea22fac6d2cdc918e
bxchang04 <bxchang04@gmail.com>
2019-11-22 02:24:01 -0500

    draft 1.9
    most likely doesnt work due to same error in 1.8

--- src/js/scripts-WIP.js
+++ src/js/scripts-WIP.js
@@ -4,8 +4,8 @@
 var pokemonRepository = (function () {
  var repository = [];
  // Creates variable for index 'ul' with pokemonList class
- var $pokemonList = document.querySelector('ul');
- var $modalContainer = document.querySelector('#modal-container');
+ var $pokemonList = $('ul');
+ var $modalContainer = $('#modal-container');
  var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   function add(pokemon) { //only accepts pokemon
@@ -13,13 +13,11 @@ var pokemonRepository = (function () {
   }
 
   function addListItem(pokemon) { //only accpets pokemon
-    var $listItem = document.createElement('li');
-    var $button = document.createElement('button');
-    $button.innerText = pokemon.name;
-    $button.classList.add('pokemon-list__button') //changed from pokemon-name
-    $listItem.appendChild($button);
+    var $listItem = $('<li></li>');
+    var $button = $('<button="pokemon-list__button">pokemon.name</button>');
+    $listItem.append($button);
     $pokemonList.appendChild($listItem);
-    $button.addEventListener('click', function() { //!!!why does this work event without a parameter in the function? - answered by Jason in submission history
+    $button.on('click', function() {
       showDetails(pokemon);
     })
   }
@@ -40,111 +38,7 @@ var pokemonRepository = (function () {
 
  // Function to load Pokemon list from API
   function loadList() {
-    return fetch(apiUrl).then(function (response) {
-      return response.json();
-    }).then(function (json) {
-      json.results.forEach(function (item) {
-        var pokemon = {
-          name: item.name.charAt(0).toUpperCase() + item.name.slice(1),
-          detailsUrl: item.url
-        };
-        add(pokemon);
-      });
-    }).catch(function (e) {
-      /* eslint-disable no-console */
-      console.error(e);
-    })
-  }
-
- // Load details of each Pokemon that is clicked
-  function loadDetails(item) {
-    var url = item.detailsUrl;
-    return fetch(url).then(function (response) {
-      return response.json();
-    }).then(function (details) {
-      // Now we add details to the item
-      item.imageUrl = details.sprites.front_default;
-      item.height = details.height;
-      // item.types = Object.keys(details.types);
-      if (details.types.length == 2 ) {
- 			item.types = [details.types[0].type.name, details.types[1].type.name];
- 		} else {
- 			item.types = [details.types[0].type.name];
- 		}
-    }).catch(function (e) {
-      console.error(e);
-    });
-  }
-
-  // Function to show modal for Pokemon data
-  var $modalContainer = document.querySelector('#modal-container');
-  var dialogPromiseReject; // This can be set later, by showDialog
-
-  function showModal(title, text) {
-    // Clear all existing modal content
-    $modalContainer.innerHTML = '';
-
-    var modal = document.createElement('div');
-    modal.classList.add('modal');
-
-    // Add the new modal content
-    var closeButtonElement = document.createElement('button');
-    closeButtonElement.classList.add('modal-close');
-    closeButtonElement.innerText = 'Close';
-    closeButtonElement.addEventListener('click', hideModal);
-
-    var titleElement = document.createElement('h1');
-    titleElement.innerText = title;
-
-    var contentElement = document.createElement('p');
-    contentElement.innerText = text;
-
-    modal.appendChild(closeButtonElement);
-    modal.appendChild(titleElement);
-    modal.appendChild(contentElement);
-    $modalContainer.appendChild(modal);
-
-    $modalContainer.classList.add('is-visible');
-  }
-
-  function hideModal() {
-    $modalContainer.classList.remove('is-visible');
-
-    if (dialogPromiseReject) {
-      dialogPromiseReject();
-      dialogPromiseRejct = null;
-    }
-  }
-
-  document.querySelector('#show-modal').addEventListener('click', () => {
-    showModal('Modal title', 'This is the modal content!');
-  });
-
-  //Modal escape methods - Check this for compatability
-  window.addEventListener('keydown', (e) => {
-    if (e.key === 'Escape' && $modalContainer.classList.contains('is-visible')) {
-      hideModal();
-    }
-  });
-
-  $modalContainer.addEventListener('click', (e) => {
-    // Since this is also triggered when clicking INSIDE the modal container,
-    // We only want to close if the user clicks directly on the overlay
-    var target = e.target;
-    if (target === $modalContainer) {
-      hideModal();
-    }
-  });
-
-  //Existing code from 1.8
-  function getAll() {
-    return repository;
-  }
-
-  function loadList() {
-    return fetch(apiUrl).then(function (response) {
-      return response.json();
-    }).then(function (json) {
+    return $.ajax(apiUrl, { dataType: 'json' }).then(function (json) {
       json.results.forEach(function (item) {
         var pokemon = {
           name: item.name,
@@ -157,11 +51,10 @@ var pokemonRepository = (function () {
     })
   }
 
+ // Load details of each Pokemon that is clicked
   function loadDetails(item) {
     var url = item.detailsUrl;
-    return fetch(url).then(function (response) {
-      return response.json();
-    }).then(function (details) {
+    return $.ajax(url, { dataType: 'json' }).then(function (details) {
       // Now we add the details to the item
       item.imageUrl = details.sprites.front_default;
       item.height = details.height;
@@ -169,6 +62,66 @@ var pokemonRepository = (function () {
     }).catch(function (e) {
       console.error(e);
     });
+  }
+
+  // Function to show modal for Pokemon data
+  var $modalContainer = $('#modal-container');
+  var dialogPromiseReject; // This can be set later, by showDialog
+
+  function showModal(title, text) {
+    // Clear all existing modal content
+    $modalContainer.innerHTML = '';
+
+    var modal = $('<div class="modal"></div>');
+
+    // Add the new modal content
+    var closeButtonElement = $('<button class="modal-close">Close</button>');
+    closeButtonElement.on('click', hideModal);
+
+    var titleElement = $('<h1>title</h1>');
+
+    var contentElement = $('<p>title</p>');
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    $modalContainer.appendChild(modal);
+
+    $modalContainer.addClass('is-visible');
+  }
+
+  function hideModal() {
+    $modalContainer.removeClass('is-visible');
+
+    if (dialogPromiseReject) {
+      dialogPromiseReject();
+      dialogPromiseRejct = null;
+    }
+  }
+
+  $('#show-modal').on('click', () => {
+    showModal('Modal title', 'This is the modal content!');
+  });
+
+  //Modal escape methods - Check this for compatability
+  window.on('keydown', (e) => {
+    if (e.key === 'Escape' && $modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
+  });
+
+  $modalContainer.on('click', (e) => {
+    // Since this is also triggered when clicking INSIDE the modal container,
+    // We only want to close if the user clicks directly on the overlay
+    var target = e.target;
+    if (target === $modalContainer) {
+      hideModal();
+    }
+  });
+
+  //Existing code from 1.8
+  function getAll() {
+    return repository;
   }
 
  return {
@@ -183,7 +136,7 @@ var pokemonRepository = (function () {
  };
 })();
 
-var $pokemonList = document.querySelector('ul');
+var $pokemonList = $('ul');
 pokemonRepository.getAll().forEach(function(pokemon) { //pokemon is placeholder name for each element in repo
   pokemonRepository.addListItem(pokemon); //what happens if parameter is blank, will it work?
 });
